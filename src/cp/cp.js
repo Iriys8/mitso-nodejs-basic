b3e2b9ef1dbafad34f93a6e0c9bdce31794c11b2
Maxim FIlyakin <iriys8@yandex.ru>
2026-02-26 12:09:42 +0300

    [Feature] cp tasks complete

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import child_process from 'child_process'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = new child_process.spawn('node', ['./src/cp/files/script.js', ...args], {stdin: ['pipe', 'pipe', 'inherit'] });
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdin);
+
+    child.on('error', (error) => {
+        console.log('error: ' + error);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2','someArgument1', 'someArgument2','someArgument1', 'someArgument2','someArgument1', 'someArgument2']);
